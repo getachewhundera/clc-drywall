e1165bb980b34ab23b7020cbc203f99c18ac9cf6
Getachew Hundera <ghundera1@gmail.com>
2023-09-04 13:06:44 -0500

    in app.jsx i started to build out the main landing page, added Our services, Recent projects, adn About Us section/>

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -26,10 +26,77 @@ function App() {
       <header>
         <Header />
         </header>
-        
+
         <body id="bodySection">
           this is the body portion
         </body>
+
+
+      <section>
+        <h1> CLC DRYWALL LLC </h1>
+        <h2> Where Quality Meets Craftsmanship.</h2> 
+        <img src="./main1.jpg"/>
+        </section>
+
+        <section>
+          <div id='mainPageServicesImage'> 
+            <img src='./truck1.jpg'/>
+          </div>
+          <div>
+            <h1> Our Services </h1>
+            <h2>
+              <ul>
+                <li> Drywall Hanging </li> 
+                <li> Drywall Finising </li>
+                <li> Popcorn Removal </li>
+                <li> Texture knowkdown </li>
+                <li> Interior Painting </li>
+                <li> and More </li>
+              </ul>
+            </h2>
+
+            <button type="button"  id="mainPageServicesButton"> Our Services </button>
+          </div>
+
+        </section>
+
+        <section> 
+          <div id='mainPageAboutUsImage'>
+            <img src='./logo.jpg'/>
+
+          </div>
+
+          <div> 
+            <h1> About Us </h1>
+            <h2>
+              We are locally owned and operated company. 
+
+
+            </h2>
+            <button type="button"  id="mainPageAboutUsButton"> About Us</button> 
+          </div> 
+
+        </section>
+
+{/* */}
+
+{/*This will be the Recent projects section where it will diplay a full project from start to finish and then link to actual recent projects page. 
+When View All button is clicked it will direct it to a Recent projects page.  */}
+        <section>
+          <h1> Recent Projects </h1>
+          <div>
+            few images will go here of one full project.
+          </div>
+            <br></br>
+
+          <button type="button"  id="mainPageViewAllButton"> View All</button> 
+
+        </section>
+
+
+
+
+
 
 
         <footer> 
