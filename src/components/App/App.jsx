dc0f40d830daefec08e8d47cb57eb546e4e7147d
Getachew Hundera <ghundera1@gmail.com>
2023-09-04 13:12:54 -0500

    added first call to action; Contact Us button on main page.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -35,6 +35,9 @@ function App() {
       <section>
         <h1> CLC DRYWALL LLC </h1>
         <h2> Where Quality Meets Craftsmanship.</h2> 
+      {/* call to action button, sits in the middle of main webpage, easily identifiable*/}   
+        <button type="button" id='mainPageContactUsButton' >  Contact Us </button>
+   
         <img src="./main1.jpg"/>
         </section>
 
