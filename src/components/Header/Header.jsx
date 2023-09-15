149154b99a7fe807d71116071cfea849a4f3ba54
Getachew Hundera <ghundera1@gmail.com>
2023-09-15 16:15:45 -0500

    added images to header.jsx and css file, added keyframes to have rotate effect but does not flip over, added split screen css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,44 @@
-import './Header.css'; 
+import './Header.css';
 
 
 
-function Header(){
-    
-    
-    console.log(window.location);
-    
-    return(
-    
-      <>
-        
-        <section>
+function Header() {
+
+
+  console.log(window.location);
+
+  return (
+
+    <>
+
+      <div className='split-screen'>
+        <div className='left-pane'>
+          <img src='/Users/getachewhundera/Documents/personal projects/clc-drywall-llc/src/components/Header/main1.jpg' alt="ClC Drywall" />
+          <img src='/Users/getachewhundera/Documents/personal projects/clc-drywall-llc/src/components/Header/nhimg1.jpeg' alt='CLC DRYWALL'/>
+          <img src='/Users/getachewhundera/Documents/personal projects/clc-drywall-llc/src/components/Header/nhimg1.2.jpeg' alt='CLC DRYWALL'/>
+
+        </div>
+
+        <div className='right-pane'>
+        <img src= "/Users/getachewhundera/Documents/personal projects/clc-drywall-llc/src/components/Header/main.jpg" alt="ClC Drywall" />
+        <img src='/Users/getachewhundera/Documents/personal projects/clc-drywall-llc/src/components/Header/nhimg1.1.jpeg' alt='CLC DRYWALL'/>
+        <img src='/Users/getachewhundera/Documents/personal projects/clc-drywall-llc/src/components/Header/nhimg1.21.jpeg' alt='CLC DRYWALL'/>
+
+        </div>
+
+        <div className='center-text'>
           <h1> CLC DRYWALL LLC </h1>
-          <h2> Where Quality Meets Craftsmanship.</h2> 
-          {/* call to action button, sits in the middle of main webpage, easily identifiable*/}   
+          <h2> Where Quality Meets Craftsmanship.</h2>
+        </div>
+        <div className='headerContactUsButton'>
+          {/* call to action button, sits in the middle of main webpage, easily identifiable*/}
           <button type="button" id='mainPageContactUsButton' >  Contact Us </button>
-    
-          <img src="./main1.jpg" alt="ClC Drywall"/>
-        </section>
+        </div>
+      </div>
 
-      </>
+    </>
 
-    );
+  );
 
 };
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,8 +7,6 @@ function Home() {
 
     return(
         <>
-
-        <h1> this is the home page </h1>
         <Header />
         <Body />
         <Footer />
