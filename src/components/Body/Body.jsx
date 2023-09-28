913b9d23bcc1752ba4914c9ff1c080c4b84291fb
Getachew Hundera <ghundera1@gmail.com>
2023-09-28 02:48:05 -0500

    added images and h2 titles in body.js, added css for images, boxes, and h2 in Body.css

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -15,27 +15,63 @@ function Body() {
         </div>
         <div id='servicesList'>
           <h1> Our Services </h1>
-         
-            <ul>
-              <li> Drywall Hanging </li>
-              <li> Drywall Finising </li>
-              <li> Popcorn Removal </li>
-              <li> Texture knowkdown </li>
-              <li> Interior Painting </li>
-              <li> and More </li>
-            </ul>
 
-            <Link to="/Services">
-              <Button variant="contained" color="primary" id="mainPageServicesButton" size='large'  
-              style={{ fontSize: '20px', padding: '12px 24px', font: 'Helvetica', fontFamily: 'sans-serif'
+          <ul>
+            <li> Drywall Hanging </li>
+            <li> Drywall Finising </li>
+            <li> Popcorn Removal </li>
+            <li> Texture knowkdown </li>
+            <li> Interior Painting </li>
+            <li> and More </li>
+          </ul>
+
+          <Link to="/Services">
+            <Button variant="contained" color="primary" id="mainPageServicesButton" size='large'
+              style={{
+                fontSize: '20px', padding: '12px 24px', font: 'Helvetica', fontFamily: 'sans-serif'
               }}> Our Services </Button>
-            </Link>
-          
-    
-      
+          </Link>
         </div>
+      </section>
+
+
+      <section id='mpContainers'> 
+
+      <div id='mpContainerOne'>
+        <img src='./veicon.png' alt='' />
+        <h2> Vast Experience </h2>
+
+      </div>
+
+      <div id='mpContainerTwo'>
+      <img src='./pwicon.png' alt='' />
+        <h2> Professional Team </h2>
+
+      </div>
+
+      <div id='mpContainerThree'>
+      <img src='./nshandshake.png' alt='' />
+        <h2> Reliable Service </h2>
+
+      </div>
+
+      <div id='mpContainerFour'>
+      <img src='./dqwicon.png' alt='' />
+        <h2> Quality Work </h2>
+
+      </div>
 
       </section>
+{/* 
+      <a href="https://www.vecteezy.com/free-vector/diamond-icon">Diamond Icon Vectors by Vecteezy</a>
+      <a href="https://www.vecteezy.com/free-vector/handshake-icon">Handshake Icon Vectors by Vecteezy</a> */}
+
+
+
+
+
+
+
 
       <section>
         <div id='mainPageAboutUsImage'>
