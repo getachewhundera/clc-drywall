5a7f3bfb775f3e29501f08fbd62af8c713a8f196
Getachew Hundera <ghundera1@gmail.com>
2023-10-17 14:22:47 -0500

    added two identical div containers with services listed for scrolling bar on top page of services in Services.jsx file

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -2,11 +2,40 @@
 import React from "react";
 
 
-function Services(){
-    return(
+function Services() {
+    return (
+        <>
+            <div class="scrollingItemsContainer">
+                <div class="horizontalScrollingItems">
+                    <div class="horizontal-scrolling-items">
+                        <> Drywall Installation </>
+                        <> Drywall Finishing </>
+                        <> Popcorn Removal </>
+                        <> Drywall Repair </>
+                        <> Texture Knockdown </>
+                        <> Interior Painting </>
 
-        <h2> this is the services page </h2>
+                    </div>
+
+                    <div class="horizontal-scrolling-items">
+                        <> Drywall Installation </>
+                        <> Drywall Finishing </>
+                        <> Popcorn Removal </>
+                        <> Drywall Repair </>
+                        <> Texture Knockdown </>
+                        <> Interior Painting </>
+
+                    </div>
+                </div>
+            </div>
+
+            <h2> this is the services page </h2>
+
+        </>
+
+
+
     )
-}; 
+};
 
 export default Services; 
