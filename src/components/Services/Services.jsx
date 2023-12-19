159f1b501d26996e97c17ea95e155fcf3d464838
Getachew Hundera <ghundera1@gmail.com>
2023-12-18 23:49:01 -0600

    added and updated images on the services page

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -38,27 +38,27 @@ function Services() {
                         <div className='itemContainer'>
                             <div className="itemTitle">Drywall Finishing</div>
                             <div className='itemDescription'>Description for Drywall Finishing</div>
-                            <div className='itemImg'> <img src='./drywallfinishing.jpg' alt='Drywall Finishing'/> </div>
+                            <div className='itemImg'> <img src='./drywallmudding.jpg' alt='Drywall Finishing'/> </div>
                         </div>
                         <div className='itemContainer'>
                             <div className="itemTitle">Popcorn Removal</div>
                             <div className='itemDescription'>Description for Popcorn Removal</div>
-                            <div className='itemImg'> <img src='./popcornremoval.jpg' alt='Popcorn Removal'/> </div>
+                            <div className='itemImg'> <img src='./ceiling_popcorn_removal.png' alt='Popcorn Removal'/> </div>
                         </div>
                         <div className='itemContainer'>
                             <div className="itemTitle">Drywall Repair</div>
                             <div className='itemDescription'>Description for Drywall Repair</div>
-                            <div className='itemImg'> <img src='./drywallrepair.jpg' alt='Drywall Repair'/> </div>
+                            <div className='itemImg'> <img src='./repairproject1.1ac.jpg' alt='Drywall Repair'/> </div>
                         </div>
                         <div className='itemContainer'>
                             <div className="itemTitle">Texture Knockdown</div>
                             <div className='itemDescription'>Description for Texture Knockdown</div>
-                            <div className='itemImg'> <img src='./textureknockdown.jpg' alt='Texture Knockdown'/> </div>
+                            <div className='itemImg'> <img src='./knockdown-texture-paint-zoomed-768x512.jpg' alt='Texture Knockdown'/> </div>
                         </div>
                         <div className='itemContainer'>
                             <div className="itemTitle">Interior Painting</div>
                             <div className='itemDescription'>Description for Interior Painting</div>
-                            <div className='itemImg'> <img src='./interiorpainting.jpg' alt='Interior Painting'/> </div>
+                            <div className='itemImg'> <img src='./interiorpaintingjpg.jpg' alt='Interior Painting'/> </div>
                         </div>
                     </div>
                 </div>
