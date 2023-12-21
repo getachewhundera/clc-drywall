cebe8565d7961af327d915beb0f65d2fe7ea19b6
Getachew Hundera <ghundera1@gmail.com>
2023-12-21 17:58:27 -0600

    moved popcorn removal image to the ServicesPageImages folder.

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -33,34 +33,34 @@ function Services() {
                         <div className='itemContainer'>
                             <div className="itemTitle">Drywall Installation</div>
                             <div className='itemDescription'>Description for Drywall Installation</div>
-                            <div className='itemImg'> <img src='./servicesPageImages/drywallinstallation.jpg' alt='Drywall Installation'/> </div>
+                            <div className='itemImg'> <img src='./ServicesPageImages/drywallinstallation.jpg' alt='Drywall Installation'/> </div>
                         </div>
                         <div className='itemContainer'>
                             <div className="itemTitle">Drywall Finishing</div>
                             <div className='itemDescription'>Description for Drywall Finishing</div>
-                            <div className='itemImg'> <img src='./servicesPageImages/drywallmudding.jpg' alt='Drywall Finishing'/> </div>
+                            <div className='itemImg'> <img src='./ServicesPageImages/drywallmudding.jpg' alt='Drywall Finishing'/> </div>
       
                         </div>
                         <div className='itemContainer'>
                             <div className="itemTitle">Popcorn Removal</div>
                             <div className='itemDescription'>Description for Popcorn Removal</div>
-                            <div className='itemImg'> <img src='./ceiling_popcorn_removal.png' alt='Popcorn Removal'/> </div>
+                            <div className='itemImg'> <img src='./ServicesPageImages/ceiling_popcorn_removal.png' alt='Popcorn Removal'/> </div>
                         </div>
                         <div className='itemContainer'>
                             <div className="itemTitle">Drywall Repair</div>
                             <div className='itemDescription'>Description for Drywall Repair</div>
-                            <div className='itemImg'> <img src='./repairproject1.1ac.jpg' alt='Drywall Repair'/> </div>
+                            <div className='itemImg'> <img src='./ServicesPageImages/repairproject1.1ac.jpg' alt='Drywall Repair'/> </div>
                         </div>
                         <div className='itemContainer'>
                             <div className="itemTitle">Texture Knockdown</div>
                             <div className='itemDescription'>Description for Texture Knockdown</div>
-                            <div className='itemImg'> <img src='./servicesPageImages/knockdown-texture.jpg' alt='Texture Knockdown'/> </div>
+                            <div className='itemImg'> <img src='./ServicesPageImages/knockdown-texture.jpg' alt='Texture Knockdown'/> </div>
       
                         </div>
                         <div className='itemContainer'>
                             <div className="itemTitle">Interior Painting</div>
                             <div className='itemDescription'>Description for Interior Painting</div>
-                            <div className='itemImg'> <img src='./interiorpaintingjpg.jpg' alt='Interior Painting'/> </div>
+                            <div className='itemImg'> <img src='./ServicesPageImages/interiorpainting.jpg' alt='Interior Painting'/> </div>
                         </div>
                     </div>
                 </div>
