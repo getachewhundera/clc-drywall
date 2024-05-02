3c1d0470d9d1afff06072533134d89fe21af9ac2
Getachew Hundera <ghundera1@gmail.com>
2024-05-01 22:49:31 -0500

    updated css and css module syntax style for landing page page.tsx and LandingPage.module.css, updated css to use bracket notation for the classNames'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,80 +3,108 @@
 'use client'
 import React from "react";
 import styles from './styles/LandingPage.module.css';
+import { StaticImageData } from "next/image";
 
 type Props = {};
 
+// Define your interface for handling images
+interface ImageProps {
+    src: string | StaticImageData;
+    alt: string;
+}
+
 
 export default function Page(props: Props) {
-    // Check if running in the browser environment
-    if (typeof window !== 'undefined') {
-        console.log(window.location.href);
-    }
 
-    const imagesLeft = [
-        '/images/main1.jpg',
-        '/images/nhimg1.jpeg',
-        '/images/nhimg1.2.jpeg'
+    // const imagesLeft: ImageProps[] = [
+    //     { src: '/images/LandingPageHeaderImages/main1.jpg', alt: "Left main image" },
+    //     { src: '/images/LandingPageHeaderImages/nhimg1.jpeg', alt: "Left header image" },
+    //     { src: '/images/LandingPageHeaderImages/nhimg1.2.jpeg', alt: "Left another header image" }
+    // ];
+
+    // const imagesRight: ImageProps[] = [
+    //     { src: '/images/LandingPageHeaderImages/main.jpg', alt: "Right main image" },
+    //     { src: '/images/LandingPageHeaderImages/nhimg1.1.jpeg', alt: "Right header image" },
+    //     { src: '/images/LandingPageHeaderImages/nhimg1.21.jpeg', alt: "Right another header image" }
+    // ];
+
+
+    // Images to be used in the left pane
+    const imagesLeft: string[] = [
+        '/images/LandingPageHeaderImages/main1.jpg',
+        '/images/LandingPageHeaderImages/nhimg1.jpeg',
+        '/images/LandingPageHeaderImages/nhimg1.2.jpeg'
     ];
+    // const imagesLeft: ImageProps[] = [
+    //     { src: '/images/LandingPageHeaderImages/main1.jpg', alt: "main image" },
+    //     { src: '/images/LandingPageHeaderImages/nhimg1.jpeg', alt: "header image" },
+    //     { src: '/images/LandingPageHeaderImages/nhimg1.2.jpeg', alt: "another header image" }
+    // ];
 
-    const imagesRight = [
+    // Images for the right pane
+    const imagesRight: string[] = [
         '/images/LandingPageHeaderImages/main.jpg',
         '/images/LandingPageHeaderImages/nhimg1.1.jpeg',
         '/images/LandingPageHeaderImages/nhimg1.21.jpeg'
     ];
 
+    // Check if running in the browser environment
+    // if (typeof window !== 'undefined') {
+    //     console.log(window.location.href);
+    // }
+
     return (
-            <div className={styles.landing_page}>
+        <div className={styles.landing_page}>
 
 
-                {/* 1st section Header banner*/}
-                <div className={styles.banner_container}>
+            {/* 1st section Header banner*/}
+            <div className={styles['banner_container']}>
 
-                    <div className={styles.splitScreen}>
-                        <div className={styles.pane + " " + styles.leftPane}>
-                            {imagesLeft.map((img, index) => (
-                                <div key={index} className={styles.image + " " + styles[`img-${index}`]} style={{ backgroundImage: `url(${img})` }}></div>
-                            ))}
-                        </div>
-
-                        <div className={styles.pane + " " + styles.rightPane}>
-                            {imagesRight.map((img, index) => (
-                                <div key={index} className={styles.image + " " + styles[`img-${index}`]} style={{ backgroundImage: `url(${img})` }}></div>
-                            ))}
-                        </div>
+                <div className={styles['split-screen']}>
+                    <div className={styles.pane + " " + styles['left-pane']}>
+                        {imagesLeft.map((img, index) => (
+                            <div key={index} className={styles.image + " " + styles[`img-${index}`]} style={{ backgroundImage: `url(${img})` }}></div>
+                        ))}
                     </div>
 
-                    <div className={styles.container}>
-                        <div className={styles.banner_content}>
-                            <span>CLC DRYWALL LLC</span>
-                            <h3>Where Quality
-                                <br />
-                                Meets
-                                <br />
-                                Craftsmanship.
-                            </h3>
-                            <div className={styles.headerContactUsButton}>
-                                {/* call to action button, sits in the middle
-                             of main webpage, easily identifiable*/}
-                                <button type="button" id={styles.mainPageContactUsButton}>
-                                    Get A Quote
-                                </button>
-                            </div>
-                        </div>
+                    <div className={styles.pane + " " + styles['right-pane']}>
+                        {imagesRight.map((img, index) => (
+                            <div key={index} className={styles.image + " " + styles[`img-${index}`]} style={{ backgroundImage: `url(${img})` }}></div>
+                        ))}
                     </div>
-
-
                 </div>
 
-                {/* 2nd section  */}
-
-
-
+                <div className={styles.container}>
+                    <div className={styles['banner_content']}>
+                        <span>CLC DRYWALL LLC</span>
+                        <h3>Where Quality
+                            <br />
+                            Meets
+                            <br />
+                            Craftsmanship.
+                        </h3>
+                        <div className={styles.headerContactUsButton}>
+                            {/* call to action button, sits in the middle
+                             of main webpage, easily identifiable*/}
+                            <button type="button" id={styles.mainPageContactUsButton}>
+                                Get A Quote
+                            </button>
+                        </div>
+                    </div>
+                </div>
 
 
             </div>
 
+            {/* 2nd section  */}
 
-       
+
+
+
+
+        </div>
+
+
+
     );
 };
