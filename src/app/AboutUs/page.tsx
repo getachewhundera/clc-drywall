8af0f459d7dd86d75ca7f06628ae86475a1e73e6
Getachew Hundera <ghundera1@gmail.com>
2024-08-06 20:48:44 -0500

    added a aboutus-description-container to hold the paragraph description and button, added a div container for the image tag

--- src/app/AboutUs/page.tsx
+++ src/app/AboutUs/page.tsx
@@ -1,14 +1,56 @@
-import './AboutUs.css';
-
+'use client'
 import React from 'react'
+
+//Styles 
+import styles from './about-us.module.css';
+
+// "Hero Section import"
+import PageHeader from '../components/PageHeader/PageHeader'
+
+import { useRouter } from 'next/navigation';
 
 type Props = {}
 
 export default function AboutUs(props: Props) {
+    const router = useRouter(); 
+
+
+
+
+
     return (
-        <>
+        <div className={styles['AboutPageBody']}>
+
+            {/* PageHeader IMPORT*/}
+            <div className={styles['about-banner-area']}>
+                <PageHeader title='About' navTitle='about' />
+            </div>
+
+            <div className={styles['aboutus-description-container']}>
+                <p>
+                    We count on 20 years of experience in the field, and our goal is to serve our clients with 
+                    responsibility and high-quality work. Our client's satisfaction is our number one priority.
+                    <br></br>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sit amet erat pretium,
+                    feugiat magna sit amet, aliquet urna. Cras quis metus ut dolor facilisis dapibus porttitor eget risus. 
+                </p>
+
+                <div className={styles['contact-button-container']}>
+                    <div className={styles.contact_aboutpage_button}>
+                        <button type="button" onClick={() => router.push('/contact')}>GET IN TOUCH</button>
+                    </div>
+                </div>
+            </div>
+
+            <div className={styles['aboutus-image']}>
+                <img src='././public/images/LandingPageAboutUsImages/logo.jpg' alt='' />
+            </div>
+
 
             <h1>Hello, This is the About Us Page!</h1>
-        </>
+
+        </div>
+
+
     )
 };
