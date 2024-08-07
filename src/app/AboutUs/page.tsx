175f70c8a1eb5d9d533c1b5aa959308bffe5cc22
Getachew Hundera <ghundera1@gmail.com>
2024-08-06 21:00:36 -0500

    created a folder inside components folder for cta component to be used globally, imported in CTACOMPONENT into about page

--- src/app/AboutUs/page.tsx
+++ src/app/AboutUs/page.tsx
@@ -7,12 +7,15 @@ import styles from './about-us.module.css';
 // "Hero Section import"
 import PageHeader from '../components/PageHeader/PageHeader'
 
+//CTA Component Import
+import CTAComponent from '../components/cta-container/cta-container';
+
 import { useRouter } from 'next/navigation';
 
 type Props = {}
 
 export default function AboutUs(props: Props) {
-    const router = useRouter(); 
+    const router = useRouter();
 
 
 
@@ -28,11 +31,11 @@ export default function AboutUs(props: Props) {
 
             <div className={styles['aboutus-description-container']}>
                 <p>
-                    We count on 20 years of experience in the field, and our goal is to serve our clients with 
+                    We count on 20 years of experience in the field, and our goal is to serve our clients with
                     responsibility and high-quality work. Our client's satisfaction is our number one priority.
                     <br></br>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sit amet erat pretium,
-                    feugiat magna sit amet, aliquet urna. Cras quis metus ut dolor facilisis dapibus porttitor eget risus. 
+                    feugiat magna sit amet, aliquet urna. Cras quis metus ut dolor facilisis dapibus porttitor eget risus.
                 </p>
 
                 <div className={styles['contact-button-container']}>
@@ -42,12 +45,14 @@ export default function AboutUs(props: Props) {
                 </div>
             </div>
 
-            <div className={styles['aboutus-image']}>
-                <img src='././public/images/LandingPageAboutUsImages/logo.jpg' alt='' />
+            <div className={styles['aboutus-image-container']}>
+                <img src='./images/LandingPageAboutUsImages/logo.jpg' alt='' />
             </div>
 
-
-            <h1>Hello, This is the About Us Page!</h1>
+            {/* CTA Component Import */}
+            <div className={styles['cta-message-container']}>
+                <CTAComponent />
+            </div>
 
         </div>
 
