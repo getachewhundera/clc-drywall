1449ccb87f8fcb852468bd84351a721e30dfae54
Getachew Hundera <ghundera1@gmail.com>
2024-08-07 21:49:20 -0500

    added css to position the about page's main-body-container and aboutus-image-container side by side.Adjusted the width and height of the image on the about page.

--- src/app/AboutUs/page.tsx
+++ src/app/AboutUs/page.tsx
@@ -29,24 +29,26 @@ export default function AboutUs(props: Props) {
                 <PageHeader title='About' navTitle='about' />
             </div>
 
-            <div className={styles['aboutus-description-container']}>
-                <p>
-                    We count on 20 years of experience in the field, and our goal is to serve our clients with
-                    responsibility and high-quality work. Our client's satisfaction is our number one priority.
-                    <br></br>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sit amet erat pretium,
-                    feugiat magna sit amet, aliquet urna. Cras quis metus ut dolor facilisis dapibus porttitor eget risus.
-                </p>
+            <div className={` ${styles['main-body-container']}`}>
+                <div className={` ${styles['aboutus-description-container']}`}>
+                    <p>
+                        We count on 20 years of experience in the field, and our goal is to serve our clients with
+                        responsibility and high-quality work. Our client's satisfaction is our number one priority.
+                        <br></br>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sit amet erat pretium,
+                        feugiat magna sit amet, aliquet urna. Cras quis metus ut dolor facilisis dapibus porttitor eget risus.
+                    </p>
 
-                <div className={styles['contact-button-container']}>
-                    <div className={styles.contact_aboutpage_button}>
-                        <button type="button" onClick={() => router.push('/contact')}>GET IN TOUCH</button>
+                    <div className={`${styles['contact-button-container']}`}>
+                        <div className={styles.contact_aboutpage_button}>
+                            <button type="button" onClick={() => router.push('/contact')}>GET IN TOUCH</button>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div className={styles['aboutus-image-container']}>
-                <img src='./images/LandingPageAboutUsImages/logo.jpg' alt='' />
+                <div className={` ${styles['aboutus-image-container']}`}>
+                    <img src='./images/LandingPageAboutUsImages/logo.jpg' alt='' />
+                </div>
             </div>
 
             {/* CTA Component Import */}
