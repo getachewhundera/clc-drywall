1b9e0fb1bb2deb2bb351ebff5e386f538f4ffa93
Getachew Hundera <ghundera1@gmail.com>
2024-08-20 10:10:08 -0500

    Imported PageHeader into contact page, added css margin-top for PageHeader, npm installed up to date, audited 427 packages in 1s
    140 packages are looking for funding
      run `npm fund` for details
    
    1 high severity vulnerability
    
    To address all issues, run:
      npm audit fix
    
    Run `npm audit` for details..

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,13 +1,26 @@
 // `app/contactus/page.tsx` is the UI for the `/contactUs` URL
-import React from 'react'
+'use client'
+// `app/projects/page.tsx` is the UI for the `/projects` URL
+import React from 'react';
+
+//CSS Styling: 
+import styles from './contact.module.css';
+
+//Component imports
+import PageHeader from '../components/PageHeader/PageHeader'
 
 type Props = {}
 
 export default function contact(props: Props) {
     return (
-        <>
 
-            <h1>Hello, This is the Contact Us Page!</h1>
-        </>
+        <div className={styles['ContactPageBody']}>
+
+        {/* PageHeader IMPORT*/}
+        <div className={styles['contact-banner-area']}>
+            <PageHeader title='Contact' navTitle='contact' />
+        </div>
+        
+      </div>  
     )
 };
